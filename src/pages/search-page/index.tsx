bec5e7429fa2e4443eff1d0a04a951e33ca3caef
DESKTOP-F1FMK7M\87344 <873440829@qq.com>
2024-11-13 00:50:45 +0800

    feat: 优化搜索为点击搜索才执行搜索

--- src/pages/search-page/index.tsx
+++ src/pages/search-page/index.tsx
@@ -1,59 +1,71 @@
 import { useState, useRef, ChangeEvent } from "react";
-import { Input, InputRef, Spin } from "antd";
+import { Input, InputRef, message, Spin } from "antd";
 import { useAssociation } from "@/state/entry/hook";
 import Icon from "@/components/icon";
 import c from "classnames";
 import s from "./index.module.less";
 
 const SearchPage = () => {
-  const [val, setVal] = useState('')
-  const [show, setShow] = useState(false)
-  const inputRef = useRef<InputRef>(null)
-  const { onAssociation, assData } = useAssociation()
+  const [val, setVal] = useState("");
+  const [show, setShow] = useState(false);
+  const inputRef = useRef<InputRef>(null);
+  const { onAssociation, assData } = useAssociation();
 
   // 搜索框变化
   const onInput = (e: ChangeEvent<HTMLInputElement>) => {
-    setVal(e.target.value)
-    setShow(!!e.target.value)
-    const value = inputRef.current?.input?.value
-    onAssociation.mutate({q: value})
-  }
+    setVal(e.target.value);
+    setShow(false);
+    // setShow(!!e.target.value);
+    // const value = inputRef.current?.input?.value;
+    // onAssociation.mutate({ q: value });
+  };
+
+  const handleSearch = () => {
+    if (!val) return message.warning("请输入搜索内容");
+    onAssociation.mutate({ q: val });
+    setShow(true);
+  };
+
   // 处理词条结果
-  const onHandleText = (data:any) => {
-    const newData = data.map((e:any) => {
-      if(e.title.includes(val)) {
-        const titleArr = e.title.split(val)
+  const onHandleText = (data: any) => {
+    const newData = data.map((e: any) => {
+      if (e.title.includes(val)) {
+        const titleArr = e.title.split(val);
         return {
           ...e,
-          title: titleArr.join(`<span style="color: #f53f3f;">${val}</span>`)
-        }
+          title: titleArr.join(`<span style="color: #f53f3f;">${val}</span>`),
+        };
       } else {
-        return e
+        return e;
       }
-    })
-    return newData || []
-  }
+    });
+    return newData || [];
+  };
 
   return (
     <div className={c(s.search_page)}>
       <div className={c(s.search_content)}>
         <Input.Search
           ref={inputRef}
-          className={s['input-wrapper']}
+          className={s["input-wrapper"]}
           placeholder="请输入搜索内容"
           enterButton="搜索"
           prefix={<Icon name="search" fill="#fff" />}
           allowClear
           value={val}
           onChange={onInput}
+          onSearch={handleSearch}
         />
-        {
-          show && <div className={c(s['association-wrapper'], 'fbv fbjc')}>
-            { onAssociation.isPending && <Spin size="large" /> }
-            { onHandleText(assData).map((e:any) => <div className={c(s['association-item'])} dangerouslySetInnerHTML={{__html: e.title}}></div>)}
-            
+        {show && (
+          <div className={c(s["association-wrapper"], "fbv fbjc")}>
+            {onAssociation.isPending && <Spin size="large" />}
+            {!onAssociation.isPending &&
+              onHandleText(assData).map((e: any) => (
+                <div className={c(s["association-item"])} dangerouslySetInnerHTML={{ __html: e.title }}></div>
+              ))}
+            {!assData.length && !onAssociation.isPending && <div className={c(s["association-item"])}>无搜索结果</div>}
           </div>
-        }
+        )}
       </div>
     </div>
   );
--- src/state/entry/hook.ts
+++ src/state/entry/hook.ts
@@ -3,11 +3,11 @@ import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
 
 export const useAssociation = () => {
-  const [assData, setAssData] = useState([])
+  const [assData, setAssData] = useState([]);
   // 联想搜索
   const onAssociation = useMutation({
     mutationFn: searchEntry,
-    onSuccess: (res:any) => setAssData(res.entries || []),
+    onSuccess: (res: any) => setAssData(res.entries || []),
   });
 
   return { onAssociation, assData };
