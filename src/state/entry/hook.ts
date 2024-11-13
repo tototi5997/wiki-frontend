5cb5e37f4b8cfed709d2dd624b44382cd9ae83be
liujiangwei <ljw2693287507@163.com>
2024-11-13 16:39:42 +0800

    feat:详情页

--- src/state/entry/hook.ts
+++ src/state/entry/hook.ts
@@ -1,14 +1,20 @@
-import { searchEntry, TypeEntry } from "@/api/entry";
+import { searchEntry, TypeEntry, getEntryDetail } from "@/api/entry";
 import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
 
-export const useAssociation = () => {
+export const useEntry = () => {
   const [assData, setAssData] = useState<TypeEntry[]>([]);
+  const [entryDetail, setEntryDetail] = useState<any>({});
   // 联想搜索
   const onAssociation = useMutation({
     mutationFn: searchEntry,
     onSuccess: (res) => setAssData(res.entries || []),
   });
+  // 词条详情
+  const onEntryDetail = useMutation({
+    mutationFn: getEntryDetail,
+    onSuccess: (res) => setEntryDetail(res || {}),
+  });
 
-  return { onAssociation, assData };
+  return { onAssociation, assData, onEntryDetail, entryDetail };
 };
